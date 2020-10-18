ce3724bd79209a1f9e9e790affc3ad0c0886d07e
mulyana24 <mulyana240498@gmail.com>
2020-10-18 20:24:14 +0700

    create button elements & test

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+import moduleName from 'elements/button'
+
+export default class LandingPage extends Component {
+    render() {
+        return ( <div> </div> )
+        }
+    }
