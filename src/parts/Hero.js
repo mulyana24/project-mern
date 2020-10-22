7efe5789889c9d92f9f603d5ab1526b40ecb2912
mulyana24 <mulyana240498@gmail.com>
2020-10-22 11:45:34 +0700

    add react-reveal and bit transition each section

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 import ImagesHero from "assets/images/picture-hero-banner.jpg";
 import FrameHero from "assets/images/frame-hero-banner.jpg";
@@ -17,74 +18,80 @@ export default function Hero(props) {
   }
 
   return (
-    <section className="container pt-4">
-      <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="font-weight-bold line-height-1 mb-3">
-            Forget Busy Work, <br />
-            Start Next Vacation
-          </h1>
-          <p
-            className="mb-4 font-weight-light text-gray 500 w-75"
-            style={{ lineHeight: "170%" }}
-          >
-            We provide what you need to enjoy your holiday with family. Time to
-            make another memorable moments.
-          </p>
-          <Button
-            className="btn px-5"
-            hasShadow
-            isPrimary
-            onClick={showMostPicked}
-          >
-            Show Me Now
-          </Button>
+    <Fade bottom>
+      <section className="container pt-4">
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 530 }}>
+            <h1 className="font-weight-bold line-height-1 mb-3">
+              Forget Busy Work, <br />
+              Start Next Vacation
+            </h1>
+            <p
+              className="mb-4 font-weight-light text-gray 500 w-75"
+              style={{ lineHeight: "170%" }}
+            >
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Button
+              className="btn px-5"
+              hasShadow
+              isPrimary
+              onClick={showMostPicked}
+            >
+              Show Me Now
+            </Button>
 
-          <div className="row" style={{ marginTop: 80 }}>
-            <div className="col-auto" styl={{ marginRight: 35 }}>
-              <img
-                width="36"
-                height="36"
-                src={IconCamera}
-                alt={`${props.data.camera}Camera`}
-              />
-              <h6 className="mt-3">
-                {formatNumber(props.data.camera)}{" "}
-                <span className="text-gray-500 font-weight-light">moments</span>
-              </h6>
+            <div className="row" style={{ marginTop: 80 }}>
+              <div className="col-auto" styl={{ marginRight: 35 }}>
+                <img
+                  width="36"
+                  height="36"
+                  src={IconCamera}
+                  alt={`${props.data.camera}Camera`}
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.camera)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    moments
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img
+                  width="36"
+                  height="36"
+                  src={IconCities}
+                  alt={`${props.data.cities}Cities`}
+                />
+                <h6 className="mt-3">
+                  {formatNumber(props.data.cities)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    cities
+                  </span>
+                </h6>
+              </div>
             </div>
-            <div className="col-auto">
+          </div>
+
+          <div className="col-6 pl-5">
+            <div style={{ width: 520, height: 410 }}>
               <img
-                width="36"
-                height="36"
-                src={IconCities}
-                alt={`${props.data.cities}Cities`}
+                src={ImagesHero}
+                alt="wedding"
+                className="img-fluid position-absolute"
+                style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
               />
-              <h6 className="mt-3">
-                {formatNumber(props.data.cities)}{" "}
-                <span className="text-gray-500 font-weight-light">cities</span>
-              </h6>
+              <img
+                src={FrameHero}
+                alt="wedding frame"
+                className="img-fluid position-absolute"
+                style={{ margin: "0 -15px -15px 0" }}
+              />
             </div>
           </div>
         </div>
-
-        <div className="col-6 pl-5">
-          <div style={{ width: 520, height: 410 }}>
-            <img
-              src={ImagesHero}
-              alt="wedding"
-              className="img-fluid position-absolute"
-              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-            />
-            <img
-              src={FrameHero}
-              alt="wedding frame"
-              className="img-fluid position-absolute"
-              style={{ margin: "0 -15px -15px 0" }}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
