d575b3e73114483967ec155dc0ce2c2921d4697b
mulyana24 <mulyana240498@gmail.com>
2020-12-27 11:28:30 +0700

    Update IconText

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -5,7 +5,7 @@ import Button from "elements/button";
 export default function IconText() {
   return (
     <Button className="brand-text-icon" href="" type="link">
-      Nabila<span className="text-gray-900">WO.</span>
+      Galih<span className="text-gray-900">WO.</span>
     </Button>
   );
 }
